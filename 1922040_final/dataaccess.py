5e59c36dab3980d984d1a7bd577af3b0271c9b40
Ryunosuke-watanabe <s1922040@stu.musashino-u.ac.jp>
2021-01-27 13:26:38 +0900

    fix dist_GA

--- 1922040_final/dataaccess.py
+++ 1922040_final/dataaccess.py
@@ -2,37 +2,37 @@ from var import Var
 from db import DB
 class DataAccess:
     def get_spots_sensitivity(self, loc):
-        query = "SELECT fun, history, view, nature FROM spot_time_table WHERE loc = %s "
+        query = "SELECT fun, history, view, nature FROM final_spot_table WHERE loc = %s "
         data = (str(loc), )
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
 
     def get_spots_sensitivity2(self, loc):
-        query = "SELECT loc, fun, history, view, nature FROM spot_time_table WHERE loc != %s "
+        query = "SELECT loc, fun, history, view, nature FROM final_spot_table WHERE loc != %s "
         data = (str(loc), )
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
 
     def get_lat_lng(self, loc):
-        query = "SELECT lat, lng FROM spot_time_table WHERE loc = %s "
+        query = "SELECT lat, lng FROM final_spot_table WHERE loc = %s "
         data = (str(loc), )
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
 
     def get_lat_lng2(self, loc):
-        query = "SELECT loc, lat, lng FROM spot_time_table WHERE loc != %s "
+        query = "SELECT loc, lat, lng FROM final_spot_table WHERE loc != %s "
         data = (str(loc), )
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
 
     def get_open_close(self, loc):
-        query = "SELECT open_time, close_time FROM spot_time_table WHERE loc = %s "
+        query = "SELECT open_time, close_time FROM final_spot_table WHERE loc = %s "
         data = (str(loc), )
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
 
     def get_open_close2(self, loc):
-        query = "SELECT loc, open_time, close_time FROM spot_time_table WHERE loc != %s "
+        query = "SELECT loc, open_time, close_time FROM final_spot_table WHERE loc != %s "
         data = (str(loc), )
         db = DB(Var.hostname, Var.port, Var.dbname, Var.username, Var.password)
         return db.execute(query, data)
